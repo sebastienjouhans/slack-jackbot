7a15cbb995f89ca99d836a1ff3f214e811e702f3
sebastien jouhans <sebastien.jouhans@gmail.com>
2018-03-18 12:02:20 +0000

    house keeping

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -1,14 +1,3 @@
-/*
-
-WHAT IS THIS?
-
-This module demonstrates simple uses of Botkit's `hears` handler functions.
-
-In these examples, Botkit is configured to listen for certain phrases, and then
-respond immediately with a single line response.
-
-*/
-
 module.exports = function(controller) {
 
     controller.hears(['^hello$'], 'direct_message,direct_mention', function(bot, message) {
