56e4df81c81bdee35390ab99351a123b62f20266
sebastien jouhans <sebastien.jouhans@gmail.com>
2018-03-19 23:41:04 +0000

    added rtm_receive_messages: false to options

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@ var bot_options = {
   clientId: process.env.slackClientId,
   clientSecret: process.env.slackClientSecret,
   debug: true,
+  rtm_receive_messages: false,
   scopes: ['bot',
             'incoming-webhook',
             'team:read',
