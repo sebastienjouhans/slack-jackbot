335725c6c52575721b38d24d435373a8177fb0c1
sebastien jouhans <sebastien.jouhans@gmail.com>
2018-03-20 21:52:36 +0000

    modified wifi to use convo

--- skills/wifi_hears.js
+++ skills/wifi_hears.js
@@ -1,11 +1,14 @@
 module.exports = function(controller, dialogflowMiddleware) {
 
     controller.hears(['wifi_intent'], 'direct_message', dialogflowMiddleware.hears, function(bot, message) {
-        controller.storage.teams.save({id: message.team, foo:'bar'}, function(err) { console.log(err) });
         console.log(message.confidence);
         if(message.confidence >.5)
         {
-            bot.reply(message, "the wifi credential for guest are\nSSID: oneConnectGuest\nPassword: 1618ac1618"); 
+            bot.startConversation(message, function(err, convo) {
+                convo.say('the wifi credential for guest are')
+                convo.say('SSID: oneConnectGuest');
+                convo.say('Password: 1618ac1618');
+            }) 
         }
         else
         {
