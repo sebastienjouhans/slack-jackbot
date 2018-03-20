5280451af2dedeeabc0be2700f9a838945c7694e
sebastien jouhans <sebastien.jouhans@gmail.com>
2018-03-20 20:35:09 +0000

    updates

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 if (!process.env.slackClientId || 
     !process.env.slackClientSecret || 
     !process.env.slackBotUserOAuthAccessToken || 
+    !process.env.dialogflowDeveloperToken || 
     !process.env.port) {
   console.log('Error: Specify variable in environment');
   process.exit(1);
@@ -44,12 +45,19 @@ var dialogflowMiddleware = require('botkit-middleware-dialogflow')({
 });
 
 // Setup for the Webserver - REQUIRED FOR INTERACTIVE BUTTONS
-// controller.setupWebserver(process.env.port, function(err,webserver) {
-//   controller.createWebhookEndpoints(controller.webserver);
-//   controller.createOauthEndpoints(controller.webserver,function(err,req,res){});
-//   //controller.startTicking();
-// });
-
+controller.setupWebserver(process.env.port, function(err,webserver) {
+  controller.createWebhookEndpoints(controller.webserver);
+  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
+    if (err) {
+      res.status(500).send('ERROR: ' + err);
+      console.log('-----------------------ERROR: ' + err);
+    } else {
+      res.send('Success!');
+      console.log('-----------------------Success!');
+    }
+  });
+  controller.startTicking();
+});
 
 controller.middleware.receive.use(dialogflowMiddleware.receive);
 slackBot.startRTM();
--- skills/help_hears.js
+++ skills/help_hears.js
@@ -2,8 +2,6 @@ module.exports = function(controller, dialogflowMiddleware) {
 
     controller.hears(['help_intent'], 'direct_message', dialogflowMiddleware.hears, function(bot, message) {
 
-console.log(message);
-
         bot.reply(message, "this is the help section, here are some example of what you can ask for.\n * 1. templates\n * 2. wifi");
         
 
