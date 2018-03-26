751b3fd787d1871c334cd400fd8076d6ee9a84e7
sebastien jouhans <sebastien.jouhans@gmail.com>
2018-03-26 21:55:17 +0100

    reverted code

--- bot.js
+++ bot.js
@@ -28,98 +28,66 @@ if (!process.env.port) {
 
 var Botkit = require('botkit');
 
-
-
-// Sample controller config - REQUIRED FOR INTERACTIVE BUTTONS
-var controller = Botkit.slackbot({
+var botkitOptions = {
+  clientId: process.env.slackClientId,
+  clientSecret: process.env.slackClientSecret,
   debug: true,
-  interactive_replies: true, // tells botkit to send button clicks into conversations
- // json_file_store: './db_slackbutton_bot/',
-}).configureSlackApp(
-  {
-    clientId: process.env.slackClientId,
-    clientSecret: process.env.slackClientSecret,
-    // Set scopes as needed. https://api.slack.com/docs/oauth-scopes
-    scopes: ['bot',
-      'incoming-webhook',
-      'team:read',
-      'users:read',
-      'users.profile:read',
-      'channels:read',
-      'im:read',
-      'im:write',
-      'groups:read',
-      'emoji:read',
-      'chat:write:bot'],
-  }
-);
+  redirectUri: 'https://slack-jackbot.herokuapp.com',
+  scopes: ['bot',
+  'incoming-webhook',
+  'team:read',
+  'users:read',
+  'users.profile:read',
+  'channels:read',
+  'im:read',
+  'im:write',
+  'groups:read',
+  'emoji:read',
+  'chat:write:bot'],
+};
 
+var slackController = Botkit.slackbot();
 
-var slackBot = controller.spawn({
-  token: process.env.slackBotUserOAuthAccessToken,
+slackController.configureSlackApp(botkitOptions);
+
+var slackBot = slackController.spawn({
+    token: process.env.slackBotUserOAuthAccessToken,
 });
 
 var dialogflowMiddleware = require('botkit-middleware-dialogflow')({
     token: process.env.dialogflowDeveloperToken,
 });
 
-controller.middleware.receive.use(dialogflowMiddleware.receive);
+slackController.middleware.receive.use(dialogflowMiddleware.receive);
+slackBot.startRTM();
 
-
-
-//slackBot.startRTM();
-
-var webserver = require(__dirname + '/components/express_webserver.js')(controller);
+var webserver = require(__dirname + '/components/express_webserver.js')(slackController);
 
 // Set up a simple storage backend for keeping a record of customers
 // who sign up for the app via the oauth
-require(__dirname + '/components/user_registration.js')(controller);
+require(__dirname + '/components/user_registration.js')(slackController);
 
 // Send an onboarding message when a new team joins
-require(__dirname + '/components/onboarding.js')(controller);
+require(__dirname + '/components/onboarding.js')(slackController);
 
 var normalizedPath = require("path").join(__dirname, "skills");
 require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./skills/" + file)(controller, dialogflowMiddleware);
+  require("./skills/" + file)(slackController, dialogflowMiddleware);
 });
 
 
 
 // Setup for the Webserver - REQUIRED FOR INTERACTIVE BUTTONS
-controller.setupWebserver(process.env.port, function(err,webserver) {
-  controller.createWebhookEndpoints(controller.webserver);
-  controller.createOauthEndpoints(controller.webserver,function(err,req,res) {
-    if (err) {
-      res.status(500).send('ERROR: ' + err);
-      debug('-----------------------ERROR: ' + err);
-    } else {
-      res.send('Success!');
-      debug('-----------------------Success!');
-    }
-  });
-  //controller.startTicking();
-});
-
-
-//REQUIRED FOR INTERACTIVE MESSAGES
-controller.storage.teams.all(function(err,teams) {
-
-  if (err) {
-    throw new Error(err);
-  }
-
-  // connect all teams with bots up to slack!
-  for (var t  in teams) {
-    if (teams[t].bot) {
-      controller.spawn(teams[t]).startRTM(function(err, bot) {
+slackController.setupWebserver(process.env.port, function(err,webserver) {
+    slackController.createWebhookEndpoints(slackController.webserver);
+    slackController.createOauthEndpoints(slackController.webserver,function(err,req,res) {
         if (err) {
-          console.log('Error connecting bot to Slack:',err);
+            res.status(500).send('ERROR: ' + err);
+            console.log('-----------------------ERROR: ' + err);
         } else {
-            console.log(bot);
-          trackBot(bot);
+            res.send('Success!');
+            console.log('-----------------------Success!');
         }
-      });
-    }
-  }
-
+    });
+    slackController.startTicking();
 });
